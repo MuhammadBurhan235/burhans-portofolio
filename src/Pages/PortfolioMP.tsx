7a82f31b36d3955a4f1b15512b436b6e1afa7c0b
MuhammadBurhan235 <muhammadburhan@student.telkomuniversity.ac.id>
2025-10-26 16:17:39 +0700

    Update minor

--- src/Pages/PortfolioMP.tsx
+++ src/Pages/PortfolioMP.tsx
@@ -576,10 +576,10 @@ function DashboardUser() {
                 <div className="bg-white p-4 md:p-6 rounded-lg shadow-[0_3px_6px_rgba(8,74,131,0.5)] mt-2 md:ml-[-70px] md:pl-18">
                   <p className="text-xs sm:text-sm md:text-base text-center">
                     I am a Bachelor’s graduate in Software Engineering from
-                    Telkom University (2025) with a strong passion for Software
-                    Developer with Agile/Lean UX Method. I have experience
-                    designing and building responsive, user-focused web
-                    interfaces through internships, professional work, and
+                    Telkom University (August 2025) with a strong passion for
+                    Software Developer with Agile/Lean UX Method. I have
+                    experience designing and building responsive, user-focused
+                    web interfaces through internships, professional work, and
                     various projects with several clients. My approach
                     emphasizes rapid iteration, validating designs through user
                     feedback, and cross-functional collaboration to deliver
